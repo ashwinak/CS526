e69869a8f9627d83225542cd3f3517432a72cb15
ashwinak <ashwinak@gmail.com>
2022-12-12 03:03:48 -0800

    module 6 - working2-final

--- src/cs526/nodeTrees/Hw6_p5.java
+++ src/cs526/nodeTrees/Hw6_p5.java
@@ -2,16 +2,14 @@ package cs526.nodeTrees;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Hw6_p5 {
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<nodeFollow> follower= new ArrayList<>();
-        allFollows("A", follower);
+        allFollows("G", follower);
     }
     public static void allFollows(String n , ArrayList<nodeFollow> follower)throws FileNotFoundException {
-//        ArrayList<nodeFollow> follower = new ArrayList<>();
         ArrayList<String> followEE;
         File graphInput = new File("src/cs526/nodeTrees/follows_input.txt");
         Scanner s = new Scanner(graphInput);
@@ -30,5 +28,88 @@ public class Hw6_p5 {
                 System.out.println(n + " directly follows " + follower.get(i).getFollows());
             }
         }
+        ArrayList<String> indirectFollowEE = new ArrayList<>();
+        ArrayList<String> visitedNodes = new ArrayList<>();
+        Stack<String> backTrack = new Stack<>();
+        ArrayList<String> temp = null;
+
+        boolean DFS = true;
+        while (DFS) {
+            for (int i=0;i<follower.size();i++) {
+                if (follower.get(i).getName().equals(n)) {
+//                    System.out.println("debug 1 " + follower.get(i).getFollows());
+                    temp = follower.get(i).getFollows();
+                    Collections.sort(temp);
+//                    for (int p=0;p<temp.size();p++) {
+//                        if(!temp.get(p).isEmpty() && temp.get(p).equals(follower.get(i).getName())) {
+//                            System.out.println("debug 5 " + follower.get(i));
+//                        }
+//                    }
+//                    System.out.println("temp is " + temp);
+                }
+            }
+            for (int i=0;i<follower.size();i++) {
+//                System.out.println("debug2 " + temp);
+                for (int j = 0; j < temp.size(); j++) {
+//                    System.out.println("temp j  " + temp.get(j));
+//                    System.out.println("follower i " + follower.get(i).getName());
+                    if (temp.get(j).strip().equals(follower.get(i).getName())) {
+                        backTrack.push(follower.get(j).getName());
+//                        System.out.println("debug3 ...");
+                        if (!follower.get(i).getFollows().isEmpty()) {
+//                            System.out.println("entering 3 ..." + follower.get(i).getFollows());
+                            for (int k = 0; k < follower.get(i).getFollows().size(); k++) {
+                                backTrack.push(follower.get(i).getName());
+                                visitedNodes.add(follower.get(i).getName());
+                                indirectFollowEE.add(k, follower.get(i).getFollows().get(k));
+                            }
+                        }
+                    }
+                }
+            }
+            DFS =false;
+
+        }
+
+        HashSet<String> indrFollow = new HashSet<>(indirectFollowEE);
+        while (true) {
+            for (int i=0;i<follower.size();i++) {
+                if (n.equals(follower.get(i).getName())) {
+                    ArrayList<String> temp2 = follower.get(i).getFollows();
+                    for (int j=0;j<temp2.size();j++) {
+//                        System.out.println(temp2.get(j));
+//                        System.out.println(indrFollow);
+                        if (indrFollow.contains(temp2.get(j))) {
+//                            System.out.println("debug 1....");
+                            indrFollow.remove(temp2.get(j));
+                        }
+                    }
+                }
+            }
+            String[] indrFollow1 = indrFollow.toArray(new String[indrFollow.size()]);
+            for (int i=0;i<indrFollow1.length;i++) {
+                for (int j=0; j<follower.size();j++) {
+//                System.out.println("debug 7 " + follower.get(j).getName());
+//                System.out.println(indrFollow1[i]);
+                    if (indrFollow1[i].strip().equals(follower.get(j).getName())) {
+                        if(!follower.get(j).getFollows().isEmpty()) {
+//                        System.out.println("debug 7 " + follower.get(j).getFollows());
+                            for (int k=0;k<follower.get(j).getFollows().size();k++) {
+                                indrFollow.add(follower.get(j).getFollows().get(k));
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i=0;i<follower.size();i++) {
+                if (n.equals(follower.get(i).getName()) && follower.get(i).getFollows().size() !=0 && !backTrack.isEmpty()) {
+                    backTrack.pop();
+                }
+            }
+            if(backTrack.isEmpty()) {
+                break;
+            }
+        }
+        System.out.println(n + " indirectly follows " + indrFollow);
     }
 }
