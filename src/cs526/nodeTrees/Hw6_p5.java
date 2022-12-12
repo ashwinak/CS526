0d2d3b03646eeba8a04a6138663dd12db12bbc18
ashwinak <ashwinak@gmail.com>
2022-12-12 14:36:54 -0800

    module 6 - removed debugs

--- src/cs526/nodeTrees/Hw6_p5.java
+++ src/cs526/nodeTrees/Hw6_p5.java
@@ -5,11 +5,16 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Hw6_p5 {
+    /**
+     * The main function will call the allFollows function with parameters such as
+     * follower and its adjaceny list where the search for direct and indirect follower is queried.
+     * A new scanner object reads the file and populates arralist of nodes with each node
+     * pointing to another array list of followees. This method creates the data structure adjacency list
+     * along with a nodeFollow class.
+     *
+     */
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<nodeFollow> follower= new ArrayList<>();
-        allFollows("D", follower);
-    }
-    public static void allFollows(String n , ArrayList<nodeFollow> follower)throws FileNotFoundException {
         ArrayList<String> followEE;
         File graphInput = new File("src/cs526/nodeTrees/follows_input.txt");
         Scanner s = new Scanner(graphInput);
@@ -23,6 +28,17 @@ public class Hw6_p5 {
             }
             follower.add(countP1++,new nodeFollow(procParse[0],followEE));
         }
+        /**
+         * This is the caller of the main function. All tests are done with this input.
+         */
+        allFollows("D", follower);
+    }
+
+    /**
+     * The function below does most of the work. For direct followee's, reading the adj list data structure
+     * is sufficient. For indirect followee's, a DFS approach is taken along with stack push and pop operation.
+     */
+    public static void allFollows(String n , ArrayList<nodeFollow> follower)throws FileNotFoundException {
         for (int i=0;i<follower.size();i++) {
             if (n.equals(follower.get(i).getName())) {
                 System.out.println(n + " directly follows " + follower.get(i).getFollows());
@@ -33,6 +49,12 @@ public class Hw6_p5 {
         Stack<String> backTrack = new Stack<>();
         ArrayList<String> temp = null;
 
+        /**
+         *  This while loop ensures the followee's adj list is stored in a seperate arraylist temp.
+         *  The temp is sorted in alphabetically so the nodes are chosen based on which alphabet or name
+         *  comes first.
+         */
+
         boolean DFS = true;
         while (DFS) {
             for (int i=0;i<follower.size();i++) {
@@ -41,6 +63,11 @@ public class Hw6_p5 {
                     Collections.sort(temp);
                 }
             }
+            /**
+             * This for loop ensures the backTrack stack is updated while traversing the graph.
+             * the first level of indirectfollowee for given user is updated by this code.
+             *
+             */
             for (int i=0;i<follower.size();i++) {
                 for (int j = 0; j < temp.size(); j++) {
                     if (temp.get(j).strip().equals(follower.get(i).getName())) {
@@ -57,6 +84,14 @@ public class Hw6_p5 {
             }
             DFS =false;
         }
+
+        /**
+         *  This while loop will recursively search for indirectfollowees no matter how far the followee is
+         *  in the directed graph.
+         *
+         *  Hash set is created to remove duplicate names in indirect followee data structure.
+         *  Any name in direct followee DS is pruned from indirect followee DS.
+         */
         HashSet<String> indrFollow = new HashSet<>(indirectFollowEE);
 
         while (true) {
@@ -82,6 +117,13 @@ public class Hw6_p5 {
                     }
                 }
             }
+
+            /**
+             * This for loop does the stack pop and creates an exit point from the while loop
+             * once everything is done. The pop occurs only when stack is not empty and adjlist is not empty
+             * and name in main function is found in adjlist.
+             */
+
             for (int i=0;i<follower.size();i++) {
                 if (n.equals(follower.get(i).getName()) && follower.get(i).getFollows().size() !=0 && !backTrack.isEmpty()) {
                     backTrack.pop();
